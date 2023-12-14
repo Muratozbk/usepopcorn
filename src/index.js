d891102065b756575e23271c51369b7982a94502
muratozbk <slipstream11@yandex.com>
2023-12-14 12:05:37 +0300

    fetch data

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import StarRating from './StarRating';
-// import './index.css';
-// import App from './App';
+// import StarRating from './StarRating';
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  const defaultTest = 5;
+import './index.css';
+import App from './App';
 
-  return (
-    <div>
-      <StarRating color='blue' maxRating={10}
-        onSetRating={setMovieRating} defaultRating={defaultTest} />
-      <p>This movie was rated {movieRating || defaultTest} stars </p>
-    </div>
-  )
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages={['Terrible',
+  <>
+    <App />
+    {/* <StarRating maxRating={5} messages={['Terrible',
       'Bad', 'Okay', 'Good', 'Amazing']} />
     <StarRating size={24} color='green' className='test'
-      defaultRating={4} />
-
-    <Test />
-  </React.StrictMode>
+      defaultRating={4} /> */}
+    {/* <Test /> */}
+  </>
 );
 
-
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   const defaultTest = 5;
+//   return (
+//     <div>
+//       <StarRating color='blue' maxRating={10}
+//         onSetRating={setMovieRating} defaultRating={defaultTest} />
+//       <p>This movie was rated {movieRating || defaultTest} stars </p>
+//     </div>
+//   )
+// }

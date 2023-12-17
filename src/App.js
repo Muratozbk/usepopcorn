b1dd28374d36b12dc411904d1ca6e34880367d99
muratozbk <slipstream11@yandex.com>
2023-12-17 12:24:37 +0300

    load movie details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRating from "./StarRating";
 
 const tempMovieData = [
   {
@@ -234,13 +235,54 @@ function Movie({ movie, onSelectMovie }) {
 };
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { Title: title, Year: year, Poster: poster,
+    Runtime: runtime, imdbRating, Plot: plot, Released: released,
+    Actors: actors, Director: director, Genre: genre } = movie;
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      setIsLoading(true)
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
+      const data = await res.json();
+      setMovie(data);
+      setIsLoading(false);
+    }
+    getMovieDetails();
+  }, [selectedId])
+
   return (
-    <div className="detail">
-      <button className="btn-back"
-        onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId} </div>
+    <div className="details">
+      {isLoading ? <Loader /> :
+        (<>
+          <header>
+            <button className="btn-back"
+              onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={title} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>{released} &bull; {runtime} </p>
+              <p>{genre} </p>
+              <p><span>⭐</span>{imdbRating} IMBD rating</p>
+            </div>
+          </header>
+
+          <section>
+            <div className="rating">
+              <StarRating maxRating={10} size={26} />
+            </div>
+            <p> <em>{plot} </em> </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>)}
+
+    </div>
   )
 }
 
